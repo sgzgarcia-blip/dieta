45a89efc20b24a0eec296b6f26eae2d489c9f577
sgzgarcia <sgzgarcia@gmail.com>
2026-02-22 18:41:08 +0100

    Updates

--- data/Default.js
+++ data/Default.js
@@ -1,164 +1,179 @@
 const alimentos = {
-   "Dieta Santi A": {
-        "Desayuno": [
-            { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
-            },
-            { "nombre": "Tortita", "kcal": 293, "proteina": 18, "carbs": 37, "grasa": 7
-            },
-            { "nombre": "Crema de cacahuetes", "kcal": 683, "proteina": 31.9, "carbs": 10.1, "grasa": 55.6
-            },
-            { "nombre": "Café", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2
-            }
-        ],
-        "Media mañana": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            }
-        ],
-        "Comida": [
-            { "nombre": "Quinoa", "kcal": 389, "proteina": 14, "carbs": 66.5, "grasa": 6.1
-            },
-            { "nombre": "Arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8
-            },
-            { "nombre": "Pollo", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
-            }
-        ],
-        "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Pechuga de pollo", "kcal": 89, "proteina": 18.1, "carbs": 1, "grasa": 1.7
-            },
-            { "nombre": "Pechuga de pavo", "kcal": 91, "proteina": 19.5, "carbs": 1, "grasa": 1.3
-            }
-        ],
-        "Cena": [
-            { "nombre": "Queso cottage", "kcal": 98, "proteina": 11.5, "carbs": 2.8, "grasa": 4.5
-            },
-            { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68
-            },
-            { "nombre": "Miel", "kcal": 332, "proteina": 0, "carbs": 83, "grasa": 0
-            }
-        ]
+    "Lunes": {
+      "Desayuno": [
+        {"nombre": "Huevos enteros", "kcal": 210, "proteina": 18, "carbs": 1, "grasa": 15},
+        {"nombre": "Avena", "kcal": 230, "proteina": 8, "carbs": 38, "grasa": 4},
+        {"nombre": "Platano", "kcal": 105, "proteina": 1, "carbs": 27, "grasa": 0}
+      ],
+      "Media mañana": [
+        {"nombre": "Yogur 0%", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Nueces", "kcal": 100, "proteina": 2, "carbs": 2, "grasa": 10}
+      ],
+      "Comida": [
+        {"nombre": "Pechuga pollo", "kcal": 165, "proteina": 35, "carbs": 0, "grasa": 3},
+        {"nombre": "Arroz crudo", "kcal": 290, "proteina": 6, "carbs": 62, "grasa": 1},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral",  "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "Merluza", "kcal": 160, "proteina": 36, "carbs": 0, "grasa": 2},
+        {"nombre": "Patata", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
     },
-  "Dieta Santi B": {
-        "Desayuno": [
-            { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
-            },
-            { "nombre": "Arandanos", "kcal": 324, "proteina": 1, "carbs": 79, "grasa": 1.4
-            },
-            { "nombre": "Avena", "kcal": 372, "proteina": 14, "carbs": 59, "grasa": 7
-            },
-            { "nombre": "Café", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2
-            }
-        ],
-        "Comida": [
-            { "nombre": "Quinoa", "kcal": 389, "proteina": 14, "carbs": 66.5, "grasa": 6.1
-            },
-            { "nombre": "Arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8
-            },
-            { "nombre": "Pollo", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
-            }
-        ],
-        "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Manzana", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
-            },
-            { "nombre": "Frutos secos", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68
-            }
-        ],
-        "Cena": [
-            { "nombre": "Caldo", "kcal": 4.2, "proteina": 0.3, "carbs": 0.2, "grasa": 0.3
-            },
-            { "nombre": "Queso en polvo", "kcal": 286, "proteina": 23.7, "carbs": 8.6, "grasa": 17.4
-            },
-            { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6
-            },
-            { "nombre": "Soja texturizada", "kcal": 361, "proteina": 50, "carbs": 13, "grasa": 8.8
-            }
-        ]
+    "Martes": {
+      "Desayuno": [
+        {"nombre": "Huevos enteros", "kcal": 210, "proteina": 18, "carbs": 1, "grasa": 15},
+        {"nombre": "Avena", "kcal": 230, "proteina": 8, "carbs": 38, "grasa": 4},
+        {"nombre": "manzana", "kcal": 80, "proteina": 0, "carbs": 21, "grasa": 0}
+      ],
+      "Media mañana": [
+        {"nombre": "queso_fresco", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Almendras", "kcal": 90, "proteina": 3, "carbs": 3, "grasa": 7}
+      ],
+      "Comida": [
+        {"nombre": "Pavo", "kcal": 165, "proteina": 35, "carbs": 0, "grasa": 2},
+        {"nombre": "Quinoa cruda", "kcal": 295, "proteina": 10, "carbs": 55, "grasa": 5},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "Salmon", "kcal": 400, "proteina": 40, "carbs": 0, "grasa": 22},
+        {"nombre": "Batata", "kcal": 220, "proteina": 4, "carbs": 50, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
     },
-   "Dieta Carla A": {
-        "Desayuno": [
-            { "nombre": "Zumo de naranja", "kcal": 45, "proteina": 0.7, "carbs": 11, "grasa": 0.1
-            },
-            { "nombre": "Cefé o poleto de té", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2
-            },
-            { "nombre": "Claras de huevo", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
-            },
-            { "nombre": "Tostada integral", "kcal": 110, "proteina": 4, "carbs": 20, "grasa": 1.5
-            }
-        ],
-        "Media mañana": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
-            }
-        ],
-        "Comida": [
-            { "nombre": "Pollo o pavo o ternera", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
-            },
-            { "nombre": "Patata cocida o arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8
-            },
-            { "nombre": "Tomate y lechuga", "kcal": 18, "proteina": 0.9, "carbs": 3.5, "grasa": 0.2
-            },
-            { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
-            }
-           
-        ],
-        "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
-            }
-        ],
-        "Cena": [
-            { "nombre": "Pescado", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3
-            },
-            { "nombre": "Verduras o Ensalada", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
-            }
-        ]
+    "Miercoles": {
+      "Desayuno": [
+        {"nombre": "Smoothie proteico", "kcal": 360, "proteina": 30, "carbs": 50, "grasa": 5}
+      ],
+      "Media mañana": [
+        {"nombre": "Yogur 0%", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Nueces", "kcal": 100, "proteina": 2, "carbs": 2, "grasa": 10}
+      ],
+      "Comida": [
+        {"nombre": "Ternera magro", "kcal": 210, "proteina": 36, "carbs": 0, "grasa": 7},
+        {"nombre": "Arroz crudo", "kcal": 290, "proteina": 6, "carbs": 62, "grasa": 1},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "pollo", "kcal": 220, "proteina": 46, "carbs": 0, "grasa": 3},
+        {"nombre": "Patata", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
     },
-    "Dieta Carla B": {
-        "Desayuno": [
-            { "nombre": "Zumo de naranja", "kcal": 45, "proteina": 0.7, "carbs": 11, "grasa": 0.1
-            },
-            { "nombre": "Cefé o poleto de té", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2
-            },
-            { "nombre": "Claras de huevo", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
-            },
-            { "nombre": "Tostada integral", "kcal": 110, "proteina": 4, "carbs": 20, "grasa": 1.5
-            },
-            { "nombre": "Crema de cacahuetes", "kcal": 683, "proteina": 31.9, "carbs": 10.1, "grasa": 55.6
-            },
-        ],
-        "Media mañana": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
-            }
-        ],
-        "Comida": [
-            { "nombre": "Pollo o pavo o ternera", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
-            },
-            { "nombre": "Patata cocida o arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8
-            },
-            { "nombre": "Tomate y lechuga", "kcal": 18, "proteina": 0.9, "carbs": 3.5, "grasa": 0.2
-            },
-            { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
-            }
-           
-        ],
-        "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
-            },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
-            }
-        ],
-        "Cena": [
-            { "nombre": "Pescado", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3
-            },
-            { "nombre": "Verduras o Ensalada", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
-            }
-        ]
+    "Jueves": {
+      "Desayuno": [
+        {"nombre": "Tortilla huevos", "kcal": 215, "proteina": 18, "carbs": 3, "grasa": 15},
+        {"nombre": "Avena", "kcal": 230, "proteina": 8, "carbs": 38, "grasa": 4}
+      ],
+      "Media mañana": [
+        {"nombre": "Whey protein", "kcal": 120, "proteina": 24, "carbs": 2, "grasa": 1},
+        {"nombre": "Nueces", "kcal": 100, "proteina": 2, "carbs": 2, "grasa": 10}
+      ],
+      "Comida": [
+        {"nombre": "Salmon", "kcal": 400, "proteina": 40, "carbs": 0, "grasa": 22},
+        {"nombre": "Arroz crudo", "kcal": 290, "proteina": 6, "carbs": 62, "grasa": 1},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "Merluza", "kcal": 160, "proteina": 36, "carbs": 0, "grasa": 2},
+        {"nombre": "Batata", "kcal": 220, "proteina": 4, "carbs": 50, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
+    },
+    "Viernes": {
+      "Desayuno": [
+        {"nombre": "Huevos enteros", "kcal": 210, "proteina": 18, "carbs": 1, "grasa": 15},
+        {"nombre": "Avena", "kcal": 230, "proteina": 8, "carbs": 38, "grasa": 4},
+        {"nombre": "Platano", "kcal": 105, "proteina": 1, "carbs": 27, "grasa": 0}
+      ],
+      "Media mañana": [
+        {"nombre": "Yogur 0%", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Almendras", "kcal": 90, "proteina": 3, "carbs": 3, "grasa": 7}
+      ],
+      "Comida": [
+        {"nombre": "Pavo", "kcal": 165, "proteina": 35, "carbs": 0, "grasa": 2},
+        {"nombre": "Quinoa cruda", "kcal": 295, "proteina": 10, "carbs": 55, "grasa": 5},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "Salmon", "kcal": 400, "proteina": 40, "carbs": 0, "grasa": 22},
+        {"nombre": "Patata", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
+    },
+    "Sábado": {
+      "Desayuno": [
+        {"nombre": "Smoothie proteico", "kcal": 360, "proteina": 30, "carbs": 50, "grasa": 5}
+      ],
+      "Media mañana": [
+        {"nombre": "Yogur 0%", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Nueces", "kcal": 100, "proteina": 2, "carbs": 2, "grasa": 10}
+      ],
+      "Comida": [
+        {"nombre": "Ternera magro", "kcal": 210, "proteina": 36, "carbs": 0, "grasa": 7},
+        {"nombre": "Arroz crudo", "kcal": 290, "proteina": 6, "carbs": 62, "grasa": 1},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "pollo", "kcal": 220, "proteina": 46, "carbs": 0, "grasa": 3},
+        {"nombre": "Batata", "kcal": 220, "proteina": 4, "carbs": 50, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
+    },
+    "Domingo": {
+      "Desayuno": [
+        {"nombre": "Huevos enteros", "kcal": 210, "proteina": 18, "carbs": 1, "grasa": 15},
+        {"nombre": "Avena", "kcal": 230, "proteina": 8, "carbs": 38, "grasa": 4},
+        {"nombre": "frutos_rojos",  "kcal": 50, "proteina": 1, "carbs": 12, "grasa": 0}
+      ],
+      "Media mañana": [
+        {"nombre": "queso_fresco", "kcal": 125, "proteina": 20, "carbs": 8, "grasa": 0},
+        {"nombre": "Almendras", "kcal": 90, "proteina": 3, "carbs": 3, "grasa": 7}
+      ],
+      "Comida": [
+        {"nombre": "Ternera magro", "kcal": 210, "proteina": 36, "carbs": 0, "grasa": 7},
+        {"nombre": "Quinoa cruda", "kcal": 295, "proteina": 10, "carbs": 55, "grasa": 5},
+        {"nombre": "Verduras mixtas", "kcal": 50, "proteina": 2, "carbs": 10, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ],
+      "Merienda": [
+        {"nombre": "Pan integral", "kcal": 150, "proteina": 6, "carbs": 30, "grasa": 2},
+        {"nombre": "Pavo", "kcal": 90, "proteina": 18, "carbs": 1, "grasa": 1}
+      ],
+      "Cena": [
+        {"nombre": "Salmon", "kcal": 400, "proteina": 40, "carbs": 0, "grasa": 22},
+        {"nombre": "Patata", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0},
+        {"nombre": "Aceite oliva", "kcal": 90, "proteina": 0, "carbs": 0, "grasa": 10}
+      ]
     }
 }

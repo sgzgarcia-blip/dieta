c1d1ccb65b4cfa7ddc10b9477ec43adcd84279e7
sgzgarcia <sgzgarcia@gmail.com>
2026-03-18 19:33:39 +0100

    Updates

--- data/Carla.js
+++ data/Carla.js
@@ -1,42 +1,42 @@
 const alimentos = {
    "Dieta Carla A": {
         "Desayuno": [
-            { "nombre": "Zumo de naranja. etc", "kcal": 45, "proteina": 0.7, "carbs": 11, "grasa": 0.1
+            { "nombre": "Zumo de naranja", "kcal": 45, "proteina": 0.7, "carbs": 11, "grasa": 0.1, "gramos": 200
             },
-            { "nombre": "Cefé o poleto de té", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2
+            { "nombre": "Cefé o poleto de té", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2, "gramos": 200
             },
-            { "nombre": "Claras de huevo", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
+            { "nombre": "Claras de huevo", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5, "gramos": 120
             },
-            { "nombre": "Tostada integral", "kcal": 110, "proteina": 4, "carbs": 20, "grasa": 1.5
+            { "nombre": "Tostada integral", "kcal": 110, "proteina": 4, "carbs": 20, "grasa": 1.5, "gramos": 30
             }
         ],
         "Media mañana": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
+            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30
             },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
+            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0, "gramos": 90
             }
         ],
         "Comida": [
-            { "nombre": "Pollo o pavo o ternera", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
+            { "nombre": "Pollo o pavo o ternera", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2, "gramos": 150
             },
-            { "nombre": "Patata cocida o arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8
+            { "nombre": "Patata cocida o arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 100
             },
-            { "nombre": "Tomate y lechuga", "kcal": 18, "proteina": 0.9, "carbs": 3.5, "grasa": 0.2
+            { "nombre": "Tomate y lechuga", "kcal": 18, "proteina": 0.9, "carbs": 3.5, "grasa": 0.2, "gramos": 150
             },
-            { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
+            { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5, "gramos": 100
             }
            
         ],
         "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
+            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30
             },
-            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0
+            { "nombre": "Manzana o Piña", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0, "gramos": 90
             }
         ],
         "Cena": [
-            { "nombre": "Pescado", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3
+            { "nombre": "Pescado", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3, "gramos": 200
             },
-            { "nombre": "Verduras o Ensalada", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5
+            { "nombre": "Verduras", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5, "gramos": 200
             }
         ]
     },

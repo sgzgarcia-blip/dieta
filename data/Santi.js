9d28e3d6608ca87305b299bfd868d4134955a2be
sgzgarcia <sgzgarcia@gmail.com>
2026-03-21 14:11:42 +0100

    Updates

--- data/Santi.js
+++ data/Santi.js
@@ -53,25 +53,27 @@ const alimentos = {
             }
         ],
         "Comida": [
-            { "nombre": "Quinoa", "kcal": 389, "proteina": 14, "carbs": 66.5, "grasa": 6.1, "gramos": 50
-            },
-            { "nombre": "Arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50
+            { "nombre": "Lentejas", "kcal": 89, "proteina": 8.2, "carbs": 10.7, "grasa": 0.4, "gramos": 210
             },
             { "nombre": "Pollo", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
             }
         ],
         "Merienda": [
             { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30
+            },
+            { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68, "gramos": 40
             }
         ],
         "Cena": [
-            { "nombre": "Queso cottage", "kcal": 98, "proteina": 11.5, "carbs": 2.8, "grasa": 4.5, "gramos": 200
+            { "nombre": "Caldo", "kcal": 4.2, "proteina": 0.3, "carbs": 0.2, "grasa": 0.3, "gramos": 120
             },
-            { "nombre": "Queso batido", "kcal": 46, "proteina": 8, "carbs": 3.5, "grasa": 0.5
+            { "nombre": "Queso en polvo", "kcal": 286, "proteina": 23.7, "carbs": 8.6, "grasa": 17.4, "gramos": 30
             },
-            { "nombre": "Cacahuetes en polvo", "kcal": 470, "proteina": 47, "carbs": 32.1, "grasa": 14, "gramos": 30
+            { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 30
             },
-            { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68, "gramos": 10
+            { "nombre": "Proteína guisante", "kcal": 347, "proteina": 50, "carbs": 17, "grasa": 5, "gramos": 100
+            },
+            { "nombre": "Proteína soja", "kcal": 324, "proteina": 51, "carbs": 20, "grasa": 0.8, "gramos": 100
             }
         ]
     },
@@ -89,9 +91,7 @@ const alimentos = {
             }
         ],
         "Comida": [
-            { "nombre": "Quinoa", "kcal": 389, "proteina": 14, "carbs": 66.5, "grasa": 6.1, "gramos": 100
-            },
-            { "nombre": "Arroz", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 100
+            { "nombre": "Garbanzos", "kcal": 90, "proteina": 5.5, "carbs": 9.5, "grasa": 2.2, "gramos": 210
             },
             { "nombre": "Pollo", "kcal": 120, "proteina": 24, "carbs": 0, "grasa": 2
             }
@@ -145,7 +145,9 @@ const alimentos = {
             },
             { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 30
             },
-            { "nombre": "Soja texturizada", "kcal": 361, "proteina": 50, "carbs": 13, "grasa": 8.8
+            { "nombre": "Proteína guisante", "kcal": 347, "proteina": 50, "carbs": 17, "grasa": 5
+            },
+            { "nombre": "Proteína soja", "kcal": 324, "proteina": 51, "carbs": 20, "grasa": 0.8
             }
         ]
     },
@@ -245,7 +247,9 @@ const alimentos = {
             },
             { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6
             },
-            { "nombre": "Soja texturizada", "kcal": 361, "proteina": 50, "carbs": 13, "grasa": 8.8
+            { "nombre": "Proteína guisante", "kcal": 347, "proteina": 50, "carbs": 17, "grasa": 5
+            },
+            { "nombre": "Proteína soja", "kcal": 324, "proteina": 51, "carbs": 20, "grasa": 0.8
             }
         ]
     },

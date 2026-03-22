48ef9a9936b94d5158d6ef9263f70c67b3ebf8e6
sgzgarcia <sgzgarcia@gmail.com>
2026-03-22 19:46:53 +0100

    Updates

--- data/Santi.js
+++ data/Santi.js
@@ -1,7 +1,7 @@
 const alimentos = {      
     "Dieta Santi A1": {
         "Desayuno": [
-            { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5, "gramos": 350
+            { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5
             },
             { "nombre": "Tortita", "kcal": 293, "proteina": 18, "carbs": 37, "grasa": 7, "gramos": 40
             },
@@ -23,7 +23,7 @@ const alimentos = {
             }
         ],
         "Merienda": [
-            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30
+            { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5
             },
             { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68, "gramos": 15
             },
@@ -33,9 +33,9 @@ const alimentos = {
         "Cena": [
             { "nombre": "Queso cottage", "kcal": 98, "proteina": 11.5, "carbs": 2.8, "grasa": 4.5, "gramos": 200
             },
-            { "nombre": "Queso batido", "kcal": 46, "proteina": 8, "carbs": 3.5, "grasa": 0.5
+            { "nombre": "Queso batido", "kcal": 46, "proteina": 8, "carbs": 3.5, "grasa": 0.5, "gramos": 200
             },
-            { "nombre": "Cacahuetes en polvo", "kcal": 470, "proteina": 47, "carbs": 32.1, "grasa": 14, "gramos": 30
+            { "nombre": "Cacahuetes en polvo", "kcal": 470, "proteina": 47, "carbs": 32.1, "grasa": 14
             },
             { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68, "gramos": 10
             }
@@ -63,7 +63,7 @@ const alimentos = {
         "Merienda": [
             { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30
             },
-            { "nombre": "Nueces", "kcal": 709, "proteina": 18, "carbs": 2.9, "grasa": 68, "gramos": 20
+            { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6
             }
         ],
         "Cena": [
@@ -71,9 +71,7 @@ const alimentos = {
             },
             { "nombre": "Queso en polvo", "kcal": 286, "proteina": 23.7, "carbs": 8.6, "grasa": 17.4, "gramos": 30
             },
-            { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 30
-            },
-            { "nombre": "Proteína guisante", "kcal": 347, "proteina": 50, "carbs": 17, "grasa": 5, "gramos": 40
+            { "nombre": "Proteína guisante", "kcal": 347, "proteina": 50, "carbs": 17, "grasa": 5
             },
             { "nombre": "Proteína soja", "kcal": 324, "proteina": 51, "carbs": 20, "grasa": 0.8, "gramos": 50
             }
@@ -109,9 +107,9 @@ const alimentos = {
         "Cena": [
             { "nombre": "Queso cottage", "kcal": 98, "proteina": 11.5, "carbs": 2.8, "grasa": 4.5, "gramos": 200
             },
-            { "nombre": "Queso batido", "kcal": 46, "proteina": 8, "carbs": 3.5, "grasa": 0.5
+            { "nombre": "Queso batido", "kcal": 46, "proteina": 8, "carbs": 3.5, "grasa": 0.5, "gramos": 200
             },
-            { "nombre": "Cacahuetes en polvo", "kcal": 470, "proteina": 47, "carbs": 32.1, "grasa": 14, "gramos": 20
+            { "nombre": "Cacahuetes en polvo", "kcal": 470, "proteina": 47, "carbs": 32.1, "grasa": 14
             },
             { "nombre": "Passas", "kcal": 324, "proteina": 3.1, "carbs": 73, "grasa": 1.1, "gramos": 20
             },

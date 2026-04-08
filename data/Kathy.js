8652fc636328429bba51e0bd4bc9729af9ae2006
sgzgarcia <sgzgarcia@gmail.com>
2026-04-08 20:50:52 +0200

    Updates

--- data/Kathy.js
+++ data/Kathy.js
@@ -1,5 +1,5 @@
 const alimentos = {
-    "Dieta Kathy": {
+    "Dieta Kathy A": {
       "dia": {
         "Desayuno": [
           { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5, "gramos": 220},
@@ -9,6 +9,112 @@ const alimentos = {
         ],
         "Media mañana": [
           { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 56},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50},
+          { "nombre": "Rodaja de piña", "kcal": 50, "proteina": 0.5, "carbs": 13, "grasa": 0.1, "gramos": 100}
+        ],
+        "Comida": [
+          { "nombre": "Pechuga Pollo", "kcal": 165, "proteina": 35, "carbs": 0, "grasa": 3, "gramos": 180},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 75},
+          { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5, "gramos": 150},
+          { "nombre": "Remolacha cocida", "kcal": 44, "proteina": 1.7, "carbs": 10, "grasa": 0.2, "gramos": 100}
+        ],
+        "Merienda": [
+          { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50}
+        ],
+        "Cena": [
+          { "nombre": "Pescado blanco", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3, "gramos": 200},
+          { "nombre": "Patata dulce", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0, "gramos": 100},
+          { "nombre": "Brócoli", "kcal": 34, "proteina": 2.8, "carbs": 7, "grasa": 0.4, "gramos": 100}
+        ]
+      },
+      "suplementos": {
+        "Lev Alginina": "3CAP",
+        "Lev Benadon": "Media",
+        "Des MultiVitaminico": "1CAP",
+        "Des BCAA": "2CAP",
+        "Des EAA": "2CAP",
+        "Des Auxina 200mg": "1CAP",
+        "Com BCAA": "2CAP",
+        "Com EAA": "2CAP",
+        "Mer BCAA": "2CAP",
+        "Mer EAA": "2CAP",
+        "Mer MultiVitaminico": "1CAP",
+        "Cen BCAA": "2CAP",
+        "Cen EAA": "2CAP",
+        "Pre Tribulus": "3CAP",
+        "Pre Glutamina": "4CAP",
+        "Pos Glutamina": "4CAP",
+        "Pos Glutamina": "3CAP",
+        "Pos BCAA": "4CAP",
+        "Pos REDOXON 1g + C": "1CAP",
+        "Pos Acetilcisteina 600mg": "1CAP",
+        "Dor Alginina": "3CAP"
+      }  
+    },
+    "Dieta Kathy B": {
+      "dia": {
+        "Desayuno": [
+          { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5, "gramos": 220},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 75},
+          { "nombre": "Fresas", "kcal": 32, "proteina": 0.7, "carbs": 7.7, "grasa": 0.3, "gramos": 100},
+          { "nombre": "Café", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2, "gramos": 120}
+        ],
+        "Media mañana": [
+          { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 56},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50},
+          { "nombre": "Rodaja de piña", "kcal": 50, "proteina": 0.5, "carbs": 13, "grasa": 0.1, "gramos": 100}
+        ],
+        "Comida": [
+          { "nombre": "Pechuga Pollo", "kcal": 165, "proteina": 35, "carbs": 0, "grasa": 3, "gramos": 180},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 75},
+          { "nombre": "Verduras cocida sin sal", "kcal": 35, "proteina": 3, "carbs": 7, "grasa": 0.5, "gramos": 150},
+          { "nombre": "Remolacha cocida", "kcal": 44, "proteina": 1.7, "carbs": 10, "grasa": 0.2, "gramos": 100}
+        ],
+        "Merienda": [
+          { "nombre": "Atún natural", "kcal": 333, "proteina": 18, "carbs": 0.5, "grasa": 0.6, "gramos": 56},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50}
+        ],
+        "Cena": [
+          { "nombre": "Pescado blanco", "kcal": 100, "proteina": 20, "carbs": 20, "grasa": 3, "gramos": 200},
+          { "nombre": "Patata dulce", "kcal": 200, "proteina": 5, "carbs": 45, "grasa": 0, "gramos": 100},
+          { "nombre": "Brócoli", "kcal": 34, "proteina": 2.8, "carbs": 7, "grasa": 0.4, "gramos": 100}
+        ]
+      },
+      "suplementos": {
+        "Lev Alginina": "3CAP",
+        "Lev Benadon": "Media",
+        "Des MultiVitaminico": "1CAP",
+        "Des BCAA": "2CAP",
+        "Des EAA": "2CAP",
+        "Des Auxina 200mg": "1CAP",
+        "Com BCAA": "2CAP",
+        "Com EAA": "2CAP",
+        "Mer BCAA": "2CAP",
+        "Mer EAA": "2CAP",
+        "Mer MultiVitaminico": "1CAP",
+        "Cen BCAA": "2CAP",
+        "Cen EAA": "2CAP",
+        "Pre Tribulus": "3CAP",
+        "Pre Glutamina": "4CAP",
+        "Pos Glutamina": "4CAP",
+        "Pos Glutamina": "3CAP",
+        "Pos BCAA": "4CAP",
+        "Pos REDOXON 1g + C": "1CAP",
+        "Pos Acetilcisteina 600mg": "1CAP",
+        "Dor Alginina": "3CAP"
+      }  
+    },
+    "Dieta Kathy C": {
+      "dia": {
+        "Desayuno": [
+          { "nombre": "Tortilla claras", "kcal": 42, "proteina": 11, "carbs": 0.5, "grasa": 0.5, "gramos": 220},
+          { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 75},
+          { "nombre": "Fresas", "kcal": 32, "proteina": 0.7, "carbs": 7.7, "grasa": 0.3, "gramos": 100},
+          { "nombre": "Café", "kcal": 2, "proteina": 6, "carbs": 4.6, "grasa": 0.2, "gramos": 120}
+        ],
+        "Media mañana": [
+          { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30},
           { "nombre": "Arroz cocido", "kcal": 350, "proteina": 7.6, "carbs": 72, "grasa": 2.8, "gramos": 50},
           { "nombre": "Rodaja de piña", "kcal": 50, "proteina": 0.5, "carbs": 13, "grasa": 0.1, "gramos": 100}
         ],

319b6471c6b4cf4f456623d964f3f9edb09dc9c1
sgzgarcia <sgzgarcia@gmail.com>
2026-07-09 15:06:05 +0200

    Updates

--- data/Santi.js
+++ data/Santi.js
@@ -18,7 +18,7 @@ const alimentos = {
             ],
             "Merienda": [
                 { "nombre": "Proteína de suero", "kcal": 42, "proteina": 78, "carbs": 3.4, "grasa": 8.5, "gramos": 30 },
-                { "nombre": "Crema de arroz", "kcal": 384, "proteina": 8, "carbs": 85, "grasa": 1, "gramos": 45 },
+                { "nombre": "Crema de arroz", "kcal": 384, "proteina": 8, "carbs": 85, "grasa": 1, "gramos": 60 },
                 { "nombre": "Crema de cacahuetes", "kcal": 683, "proteina": 31.9, "carbs": 10.1, "grasa": 55.6 },
                 { "nombre": "Manzana", "kcal": 52, "proteina": 0.3, "carbs": 22, "grasa": 0, "gramos": 120 }
             ],
